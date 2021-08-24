9c617535ce7e96dbbd60c22171b4e67ca2638aee
Katia <katerina.bogrom@gmail.com>
2021-08-24 22:25:59 +0200

    ejercicio 10 listo

--- ejercicio6-10/index.js
+++ ejercicio6-10/index.js
@@ -1,13 +1,13 @@
 
 if (document.location.pathname == "/ejercicio6-10/especies.html") {
 
-    document.getElementsByClassName("button")[0].style.backgroundColor = "rgb(204, 255, 153)";
-    document.getElementsByClassName("button")[1].style.backgroundColor = "rgb(212, 192, 172)";
+    document.getElementsByClassName("button")[0].style.background = "linear-gradient(to bottom, white 15%, rgb(204, 255, 153))";
+    document.getElementsByClassName("button")[1].style.background = "linear-gradient(to bottom, white 15%, rgb(212, 192, 172))";
 
 
 } else if (document.location.pathname == "/ejercicio6-10/index.html") {
-    document.getElementsByClassName("button")[0].style.backgroundColor = "rgb(212, 192, 172)";
-    document.getElementsByClassName("button")[1].style.backgroundColor = "rgb(204, 255, 153)";
+    document.getElementsByClassName("button")[0].style.background = "linear-gradient(to bottom, white 15%, rgb(212, 192, 172))";
+    document.getElementsByClassName("button")[1].style.background = "linear-gradient(to bottom, white 15%, rgb(204, 255, 153))";
 }
 
 
